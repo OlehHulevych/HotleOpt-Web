27b5ecd9f74116c40871e568ab0f289f6ed1f249
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-07-28 10:13:00 +0200

    added image compression

--- src/api/rooms.ts
+++ src/api/rooms.ts
@@ -1,5 +1,6 @@
 import client from './client'
 import type {Room, PaginatedResult, RoomStatus, RoomPhoto, RoomType} from '../types/room'
+import imageCompression from "browser-image-compression";
 
 export const getRooms = (page = 1, pageSize = 20) =>
   client
@@ -14,8 +15,15 @@ export const getRoomsByProperty = (propertyId: string, status?: RoomStatus, page
     .then((r) => r.data.result)
 export const getPhotosByRoom = (roomId:string, page=1, pageSize=20) => client.get<PaginatedResult<RoomPhoto>>(`/api/rooms/${roomId}/photos`, {params:{page,pageSize}}).then(r=>r.data)
 export const addRoom = (dto:{roomNumber:string, description:string, propertyId:string, type:RoomType, pricePerNight:number }) => client.post('api/room/', dto).then(r=>r.data);
-export const uploadPhoto = (roomId:string, file:File) =>{
+export const uploadPhoto = async (roomId:string, file:File) =>{
     const form = new FormData();
-    form.append('file', file)
+    const options  = {
+        maxSizeMb:1,
+        maxWidthOrHeight:1920,
+        useWebHooker:true
+    };
+
+    const compressedFile = await imageCompression(file,options);
+    form.append('file', compressedFile)
     return client.post(`/api/rooms/${roomId}/photos`, form);
 }
